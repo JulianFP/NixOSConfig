a8d138495650673fa52a6abaddc93683149ad9e2
Julian Partanen <70963316+JulianFP@users.noreply.github.com>
2023-09-05 15:38:22 +0200

    Update networking.nix

--- networking.nix
+++ networking.nix
@@ -22,42 +22,4 @@
   };
 
   # Wireguard and other VPNs are added through networkmanager and not declaratevely through Nix
-
-  # nebula config
-  services.nebula.networks."serverNetwork" = {
-    enable = true;
-    ca = /root/.nebula/ca.crt;
-    key = /root/.nebula/JuliansFramework.key;
-    cert = /root/.nebula/JuliansFramework.crt;
-    listen.port = 51821;
-    lighthouses = [ "48.42.0.1" ];
-    staticHostMap = {
-      "48.42.0.1" = [
-        "82.165.49.241:51821"
-      ];
-    };
-    settings = {
-      cipher = "aes";
-      punchy = {
-        punch = true;
-        respond = true;
-      };
-    };
-    firewall = {
-      outbound = [
-        {
-          host = "any";
-          port = "any";
-          proto = "any";
-        }
-      ];
-      inbound = [
-        {
-          port = "any";
-          proto = "icmp";
-          host = "any";
-        }
-      ];
-    };
-  };
 }
