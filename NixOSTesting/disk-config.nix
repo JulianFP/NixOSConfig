6f6665035c24f8bf9bc18cd72ddaba31e2b53303
JulianFP <julian@partanengroup.de>
2023-09-17 20:51:21 +0200

    fixed partition table

--- NixOSTesting/disk-config.nix
+++ NixOSTesting/disk-config.nix
@@ -11,6 +11,16 @@
           size = "1M";
           type = "EF02"; #for grub MBR
         };
+        esp = {
+          name = "ESP";
+          size = "500M";
+          type = "EF00";
+          content = {
+            type = "filesystem";
+            format = "vfat";
+            mountpoint = "/boot";
+          };
+        };
         root = {
           name = "root";
           size = "100%";
