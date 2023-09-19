5ea39506ed91aced92beebb440a144c302998f20
JulianFP <julian@partanengroup.de>
2023-09-19 17:24:51 +0200

    added nebula option to deployment and changed IonosVPS

--- IonosVPS/configuration.nix
+++ IonosVPS/configuration.nix
@@ -21,6 +21,7 @@
   environment.systemPackages = with pkgs; [
     vim
     wget
+    git
   ];
 
   services.qemuGuest.enable = true;
--- deployment.sh
+++ deployment.sh
@@ -17,38 +17,42 @@ nebulaFilesPath="nebulaDevice" #path to nebula crt and key (without .crt and .ke
 echoerr() { echo "$@" 1>&2; }
 
 help() {
-    printf "usage: ./deployment.sh <option> <flakeHostname> [<currentTargetIP>] [<futureTargetIP>] [<nebula name>] [<nebula ip>] [<nebula groups>]\n\n"
+    printf "general usage: ./deployment.sh <option> [...]\n\n"
+    printf "usage (option: deploy): ./deployment.sh deploy <flakeHostname> <currentTargetIP> <futureTargetIP>\n\n"
+    printf "usage (option: deployNebula): ./deployment.sh deployNebula <flakeHostname> <currentTargetIP> <futureTargetIP> <nebula name> <nebula ip> <nebula groups>\n\n"
+    printf "usage (option: nebula): ./deployment.sh nebula <currentTargetIP> <nebula name> <nebula ip> <nebula groups>\n\n"
+    printf "usage (option: iso): ./deployment.sh iso <flakeHostname>\n\n"
     echo "option:"
-    echo "   deploy   deployment without nebula using nixos-anywhere"
-    echo "   nebula   deployment with nebula using nixos-anywhere"
-    echo "   iso      just builds an iso containing the config without nebula"
-    echo "   Requirements for options deploy and nebula:"
+    echo "   deploy         deployment without nebula using nixos-anywhere"
+    echo "   deployNebula   deployment with nebula using nixos-anywhere"
+    echo "   nebula         configure nebula on existing nixos target (no deploy)"
+    echo "   iso            just builds an iso containing the config without nebula"
+    echo "   Requirements for options deploy and deployNebula:"
     echo "      - x86_64 VM (possibly others, not tested)"
     echo "      - root ssh access (with ssh key)"
     echo "      - configuration for target must be flake with disko and nix-command"
     echo "      - at least 1.5GB RAM (without swap), OR"
-    printf "        currently booted from nixos live cd (not running on target drive)\n\n"
+    echo "        currently booted from nixos live cd (not running on target drive)"
+    echo "   Requirements for option nebula:"
+    echo "      - root ssh access (with ssh key)"
+    printf "      - target machine runs nixos with flake configured at top of this script and has nebula module already setup\n\n"
     echo "flakeHostname:"
     echo "   name of target machine used for flake url (the part after the '#' in flake url)"
     printf "   for example: NixOSTesting\n\n"
     echo "currentTargetIP:"
-    echo "   specify only when using deploy or nebula option"
     echo "   ip address that the target currently has"
     printf "   the ssh server on the target has to accessible over this ip\n\n"
     echo "futureTargetIP:"
-    echo "   specify only when using deploy or nebula option"
     echo "   ip address that the target will have when configuration is applied"
     echo "   useful if configuration specifies ip different from current ip"
     printf "   will be the same than currentTargetIP in most cases\n\n"
     echo "nebula name:"
-    echo "   specify only when using nebula option"
     printf "   name of device in nebula network\n\n"
     echo "nebula ip:"
-    echo "   specify only when using nebula option"
     printf "   ip of device in nebula network (with prefix length)\n\n"
     echo "nebula groups:"
-    echo "   specify only when using nebula option"
-    echo "   groups of device in nebula network (can be empty)"
+    echo "   groups of device in nebula network"
+    echo "   pass an empty string if you don't want the target to be in any group"
 }
 
 privileges() {
@@ -60,38 +64,40 @@ privileges() {
     fi
 }
 
+#$1: flakehostname, $2: currentTargetIP, $3: futureTargetIP
 deploy() {
     #check if enough parameters are provided
-    if [[ $# < 4 ]]; then
+    if [[ $# < 3 ]]; then
         echoerr "Missing parameters. use help option to find out how to use this script"
         exit 1
     fi
 
     #check if device is reachable over ssh and wait until user fixed it
-    until ssh -o "StrictHostKeyChecking no" root@$3 true >/dev/null 2>&1; do 
+    until ssh -o "StrictHostKeyChecking no" root@$2 true >/dev/null 2>&1; do 
         echo "couldn't connect to target machines root user over ssh."
         read -p "check ssh config and then press enter to try again"
     done
 
     # run nixos-anywhere
-    nix run github:numtide/nixos-anywhere -- --flake "github:$githubRepo#$2" root@$3
+    nix run github:numtide/nixos-anywhere -- --flake "github:$githubRepo#$1" root@$2
 
     #delete futureTargetIP ssh known_hosts to prevent error messages in terminal
-    ssh-keygen -R "$4"
+    ssh-keygen -R "$3"
 
     #wait until device becomes reachable over ssh with new ip address
     echo "wait for vm to become reachable over ssh and new ip address"
-    until ssh -o "StrictHostKeyChecking no" root@$4 true >/dev/null 2>&1; do 
+    until ssh -o "StrictHostKeyChecking no" root@$3 true >/dev/null 2>&1; do 
         sleep 1 
     done
 
     #git clone nix configuration onto target to enable changing configuration directly on target machine
-    ssh root@$4 -o "StrictHostKeyChecking no" "nix shell nixpkgs#git -c git clone https://github.com/$githubRepo.git /etc/nixos"
+    ssh root@$3 -o "StrictHostKeyChecking no" "nix shell nixpkgs#git -c git clone https://github.com/$githubRepo.git /etc/nixos"
 }
 
+#$1 flakehostname
 iso() {
     #check if enough parameters are provided
-    if [[ $# < 2 ]]; then
+    if [[ $# < 1 ]]; then
         echoerr "Missing parameters. use help option to find out how to use this script"
         exit 1
     fi
@@ -99,30 +105,34 @@ iso() {
     #generate iso name for symlink. This takes existing files into consideration:
     #if file "$2.iso" already exists, it will append a number
     #this number will get larger as long as it needs to in order to find an unused file name
-    isoname="$2.iso"
+    isoname="$1.iso"
     path=$(pwd)
     fileNum=2
     while ls $path | grep -q "$isoname"; do
-        isoname="$2-$fileNum.iso"
+        isoname="$1-$fileNum.iso"
         ((++fileNum))
     done
 
     #run generation script and inform user about output file name
-    nix run github:nix-community/nixos-generators -- -f iso -o "$path/$isoname" --flake "github:$githubRepo#$2"
+    nix run github:nix-community/nixos-generators -- -f iso -o "$path/$isoname" --flake "github:$githubRepo#$1"
     echo "you can find your iso in $path/$isoname"
 }
 
+#$1: currentTargetIP, $2: nebula name, $3: nebula ip, $4: nebula groups
 nebula() {
     #check if enough parameters are provided
-    if [[ $# < 6 ]]; then
+    if [[ $# < 4 ]]; then
         echoerr "Missing parameters. use help option to find out how to use this script"
         exit 1
     fi
 
+    #get current hostname of target for nebula key name
+    hostname=$(ssh root@$1 -o "StrictHostKeyChecking no" "hostnamectl hostname")
+
     #create .nebula folder in /root (this will also serve as backup for nebula files)
-    ssh root@$4 -o "StrictHostKeyChecking no" "mkdir /root/.nebula"
+    ssh root@$1 -o "StrictHostKeyChecking no" "mkdir /root/.nebula"
     #generate private/public keypair on target
-    ssh root@$4 -o "StrictHostKeyChecking no" "nix shell nixpkgs#nebula -c nebula-cert keygen -out-key /root/.nebula/$nebulaFilesPath.key -out-pub /root/.nebula/$nebulaFilesPath.pub"
+    ssh root@$1 -o "StrictHostKeyChecking no" "nix shell nixpkgs#nebula -c nebula-cert keygen -out-key /root/.nebula/$nebulaFilesPath.key -out-pub /root/.nebula/$nebulaFilesPath.pub"
 
     #wait until usb stick with ca.key is present
     until [[ -e "$luksUSBDevice" ]]; do 
@@ -134,25 +144,25 @@ nebula() {
     mount /dev/mapper/luksUSBDeviceNebula /mnt 
 
     #similar to iso name generation in iso function: find free filename for crt file on usb stick
-    nebname="$2"
+    nebname="$hostname"
     fileNum=2
     while ls "/mnt/$luksUSBNebulaPath" | grep -q "$nebname"; do
-        nebname="$2-$fileNum"
+        nebname="$hostname-$fileNum"
         ((++fileNum))
     done
 
     #copy public key from target machine and sign it locally with ca.key from usb stick
-    scp root@$4:/root/.nebula/$nebulaFilesPath.pub "/mnt/$luksUSBNebulaPath/$nebname.pub"
-    nix shell nixpkgs\#nebula -c nebula-cert sign -ca-crt "/mnt/$luksUSBNebulaPath/ca.crt" -ca-key "/mnt/$luksUSBNebulaPath/ca.key" -in-pub "/mnt/$luksUSBNebulaPath/$nebname.pub" -out-crt "/mnt/$luksUSBNebulaPath/$nebname.crt" -name $5 -ip $6 -groups $7
+    scp root@$1:/root/.nebula/$nebulaFilesPath.pub "/mnt/$luksUSBNebulaPath/$nebname.pub"
+    nix shell nixpkgs\#nebula -c nebula-cert sign -ca-crt "/mnt/$luksUSBNebulaPath/ca.crt" -ca-key "/mnt/$luksUSBNebulaPath/ca.key" -in-pub "/mnt/$luksUSBNebulaPath/$nebname.pub" -out-crt "/mnt/$luksUSBNebulaPath/$nebname.crt" -name $2 -ip $3 -groups $4
 
     #copy crt file to target machine and also copy them into place in /etc/nixos 
-    scp "/mnt/$luksUSBNebulaPath/$nebname.crt" root@$4:/root/.nebula/$nebulaFilesPath.crt
-    ssh root@$4 -o "StrictHostKeyChecking no" "cat /root/.nebula/$nebulaFilesPath.crt > /etc/nixos/$nebulaFilesPath.crt"
-    ssh root@$4 -o "StrictHostKeyChecking no" "cat /root/.nebula/$nebulaFilesPath.key > /etc/nixos/$nebulaFilesPath.key"
+    scp "/mnt/$luksUSBNebulaPath/$nebname.crt" root@$1:/root/.nebula/$nebulaFilesPath.crt
+    ssh root@$1 -o "StrictHostKeyChecking no" "cat /root/.nebula/$nebulaFilesPath.crt > /etc/nixos/$nebulaFilesPath.crt"
+    ssh root@$1 -o "StrictHostKeyChecking no" "cat /root/.nebula/$nebulaFilesPath.key > /etc/nixos/$nebulaFilesPath.key"
 
     #apply nebula config and configure git to ignore changed keyfiles to prevent them from being overriden or pushed to github
-    ssh root@$4 -o "StrictHostKeyChecking no" "nixos-rebuild switch"
-    ssh root@$4 -o "StrictHostKeyChecking no" "nix shell nixpkgs#git -c sh -c 'cd /etc/nixos && git update-index --skip-worktree $nebulaFilesPath.*'"
+    ssh root@$1 -o "StrictHostKeyChecking no" "nixos-rebuild switch"
+    ssh root@$1 -o "StrictHostKeyChecking no" "nix shell nixpkgs#git -c sh -c 'cd /etc/nixos && git update-index --skip-worktree $nebulaFilesPath.*'"
 
     #unmount and lock usb stick again
     umount /mnt 
@@ -162,15 +172,21 @@ nebula() {
 case $1 in 
     deploy)
     privileges "$@"
-    deploy "$@"
+    deploy "$2" "$3" "$4"
     echo "deployment completed"
+    exit 0
+        ;;
+    deployNebula)
+    privileges "$@"
+    deploy "$2" "$3" "$4"
+    nebula "$3" "$5" "$6" "$7" "$8"
+    echo "deployment with nebula completed"
     exit 0
         ;;
     nebula)
     privileges "$@"
-    deploy "$@"
     nebula "$@"
-    echo "deployment with nebula completed"
+    echo "nebula configured"
     exit 0
         ;;
     iso)
