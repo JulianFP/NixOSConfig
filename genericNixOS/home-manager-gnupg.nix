732f7a87498455bfe698d9938d6d953fae6aae61
JulianFP <julian@partanengroup.de>
2023-09-21 04:29:39 +0200

    fixed gnupg hm

--- genericNixOS/home-manager-gnupg.nix
+++ genericNixOS/home-manager-gnupg.nix
@@ -4,20 +4,22 @@
   imports = [
     inputs.home-manager-stable.nixosModules.home-manager 
     {
-      useGlobalPkgs = true;
-      useUserPackages = true;
-      users.root = {
-        programs.gpg = {
-          enable = true;
-          publicKeys = [{
-            source = ../../../gpg_yubikey.asc;
-            trust = 5;
-          }];
-          settings.no-autostart = true;
-        };
+      home-manager = {
+        useGlobalPkgs = true;
+        useUserPackages = true;
+        users.root = {
+          programs.gpg = {
+            enable = true;
+            publicKeys = [{
+              source = ../../../gpg_yubikey.asc;
+              trust = 5;
+            }];
+            settings.no-autostart = true;
+          };
 
-        home.stateVersion = "23.05";
-        programs.home-manager.enable = true;
+          home.stateVersion = "23.05";
+          programs.home-manager.enable = true;
+        };
       };
     }
   ];
