af4343623c88fd57874c7cff270ed0282fa3d5a0
JulianFP <julian@partanengroup.de>
2023-10-01 15:29:42 +0200

    improved suspend script

--- JuliansFramework/home-manager/julian/systemScripts/lockAndSuspend.sh
+++ JuliansFramework/home-manager/julian/systemScripts/lockAndSuspend.sh
@@ -2,5 +2,9 @@
 swaylockProcesses=$(ps aux | grep -c swaylock)
 if [ $swaylockProcesses -le 1 ]; then
     swaylock -f -c 000000 -i $1
+
+ #do not suspend if machine already woke up recently (during the current minute). Makes it possible to wake machine pressing the power button
+elif journalctl -u sleep.target -S $(date +%H:%M) | grep -q "Stopped"; then
+    exit 0
 fi
-systemctl suspend
+systemctl suspend -i
