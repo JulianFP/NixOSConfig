3a20ff8e320c86909d70a265848a0cf423fb55d2
JulianFP <julian@partanengroup.de>
2024-01-28 21:35:51 +0100

    server: add automatic optimisation of nix store and polish other automatic maintenance services

--- generic/server.nix
+++ generic/server.nix
@@ -23,25 +23,30 @@
   #vm stuff
   services.qemuGuest.enable = true;
 
+  #automatic maintenance services
+  #automatic garbage collect to avoid storage depletion by autoUpgrade
+  nix.gc = {
+    automatic = true;
+    dates = "03:00";
+    randomizedDelaySec = "30min";
+    options = "--delete-older-than 30d";
+  };
+  #automatic optimisation of nix store to save even more storage 
+  nix.optimise = {
+    automatic = true;
+    dates = [ "03:45" ];
+  };
   #automatic upgrade. Pulls newest commits from github daily. Relies on my updating the flake inputs (I want that to be manual and tracked by git)
   system.autoUpgrade = {
     enable = true;
     flake = "github:JulianFP/NixOSConfig";
-    dates = "03:00";
-    randomizedDelaySec = "60min";
+    dates = "04:00";
+    randomizedDelaySec = "30min";
     allowReboot = true;
     rebootWindow = {
-      lower = "03:00";
+      lower = "04:00";
       upper = "05:00";
     };
-
-  };
-  #automatic garbage collect to avoid storage depletion by autoUpgrade
-  nix.gc = {
-    automatic = true;
-    dates = "04:00";
-    randomizedDelaySec = "60min";
-    options = "--delete-older-than 30d";
   };
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
