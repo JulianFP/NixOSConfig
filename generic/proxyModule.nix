d60806f3ed218c7eaa0f084536bb760ece7f60ac
JulianFP <julian@partanengroup.de>
2025-02-08 03:52:58 +0100

    proxy: Fix startup order on edge host

--- generic/proxyModule.nix
+++ generic/proxyModule.nix
@@ -87,6 +87,10 @@ in
       bind = "127.0.0.1 ${edgeNebulaIP}";
       requirePassFile = config.sops.secrets."redis/caddy-server".path;
     };
+    systemd.services."redis.caddy-storage" = lib.mkIf cfg.isEdge {
+      after = [ "nebula@serverNetwork.service" ];
+      before = [ "caddy.service" ];
+    };
 
     services.caddy = {
       enable = true;
