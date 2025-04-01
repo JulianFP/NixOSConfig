bfbdf11d3501d844d824e526e2ca2a57d97444f0
JulianFP <julian@partanengroup.de>
2025-04-01 16:57:17 +0200

    desktop: Add nix-ld as a workaround for standard Python development with pip

--- generic/desktop/desktop.nix
+++ generic/desktop/desktop.nix
@@ -111,6 +111,12 @@ structure:
       enable = true;
       openFirewall = true;
     };
+    nix-ld = {
+      enable = true;
+      libraries = with pkgs; [
+        zlib zstd stdenv.cc.cc curl openssl attr libssh bzip2 libxml2 acl libsodium util-linux xz systemd
+      ];
+    };
   };
 
 
