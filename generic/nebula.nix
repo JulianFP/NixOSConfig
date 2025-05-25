7ce70f519cbb1aab3f25d6fbbc58035f9d318742
JulianFP <julian@partanengroup.de>
2025-05-25 21:27:39 +0200

    backupServer: Add to nebula.nix

--- generic/nebula.nix
+++ generic/nebula.nix
@@ -11,6 +11,7 @@
       #servers
       mainserver = "48.42.0.2";
       IonosVPS = "48.42.0.5";
+      backupServer = "48.42.0.7";
       #containers
       Nextcloud = "48.42.1.131";
       Nextcloud-Testing = "48.42.1.150";
