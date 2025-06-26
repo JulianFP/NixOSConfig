7715f538e2ec910fce5e0fd59ff2c7eaba1b4f78
JulianFP <julian@partanengroup.de>
2025-06-26 22:12:46 +0200

    shell: Add second urz runner to ssh match block

--- genericHM/shell.nix
+++ genericHM/shell.nix
@@ -48,6 +48,10 @@
         hostname = "129.206.119.205";
         user = "localadmin";
       };
+      "project-w-runner_urz_2" = {
+        hostname = "129.206.119.206";
+        user = "localadmin";
+      };
       "ssc-whisper" = {
         hostname = "ssc-whisper.iwr.uni-heidelberg.de";
         user = "jpartanen";
